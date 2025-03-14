c7dc262585b4d4974eae384283f8fe17eac8c789
Denis Kolaj <deniskolaj2@gmail.com>
2025-03-14 23:47:30 +0100

    create TaskItem class

---
+++ TaskManagerConsole/TaskItem.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TaskManagerConsole
+{
+    class TaskItem
+    {
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
