c6369ee264a6c51662d4a60d239b8a22284f5a57
Denis Kolaj <deniskolaj2@gmail.com>
2025-03-15 22:53:12 +0100

    implement "load" task command

--- TaskManagerConsole/Program.cs
+++ TaskManagerConsole/Program.cs
@@ -1,7 +1,39 @@
-﻿class Program
+﻿using System.Text.Json;
+using TaskManagerConsole;
+
+class Program
 {
+    static readonly string SaveFile = "tasks.json";
+    static List<TaskItem> tasks = new();
+
     static void Main()
     {
-        Console.WriteLine("Task Manager Console");
+        LoadTasks();
+        Console.WriteLine("\n----- Task Manager Console -----\n");
+
+        while (true) {
+            Console.Write("Enter command: ");
+            string? command = Console.ReadLine()?.Trim().ToLower();
+
+            switch (command) 
+            {
+                case "load":
+                    LoadTasks();
+                    break;
+                default:
+                    Console.WriteLine("Unknown command. Try again.");
+                    break;
+            }
+        }
+    }
+
+    static void LoadTasks()
+    {
+        if (File.Exists(SaveFile))
+        {
+            string json = File.ReadAllText(SaveFile);
+            tasks = JsonSerializer.Deserialize<List<TaskItem>>(json) ?? new List<TaskItem>();
+            Console.WriteLine("Tasks loaded.");
+        }
     }
 }
